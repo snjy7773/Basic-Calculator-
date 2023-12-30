8beb8164fbd7ea36b95d97bd6ac801959aa2c8b0
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-30 07:49:22 +0530

    w...

--- main.js
+++ main.js
@@ -2,16 +2,24 @@ let abt = document.querySelectorAll("button");
 let int = document.querySelector("#int");
 
 var currentInput = "" ;
-
+var clear = "";
 
 
 function btnsvl(num){
    
     currentInput += num;
-    updateDisplay()
+    int.value = currentInput
+    // updateDisplay()
      
 
 }
-function updateDisplay() {
-    int.value = currentInput || '0';
+// function updateDisplay() {
+//     int.value = currentInput || '0';
+// }
+
+function clear(a) {
+    
+   
+    
 }
+    
