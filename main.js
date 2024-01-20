af75a3b76f76f5cae286ad53f5df642e6990187a
snjy7773 <sanjay1993sanjay1993@gmail.com>
2024-01-20 22:27:43 +0530

    done basic calc
    not do the indepth  method . we use simple calc use fn  eval( )  method

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 let abt = document.querySelectorAll("button");
 let int = document.querySelector("#int");
 
-var currentInput = "" ;
-var clear = "";
+// var currentInput = "";
 
-
-function btnsvl(num){
-   
-    currentInput += num;
-    int.value = currentInput
-    // updateDisplay()
-     
-
+function btnsvl(vl) {
+  // currentInput += vl;
+  // int.value = currentInput;
+  int.value += vl;
 }
-// function updateDisplay() {
-//     int.value = currentInput || '0';
+
+function clean() {
+  // int.value = null;
+  int.value = "";
+  // currentInput ="";
+}
+
+// function calculateResult() {
+//   try {
+//     // Evaluate the expression using eval() function
+//     int.value = eval(int.value);
+//   } catch (abt) {
+//     int.value = "error";
+//   }
 // }
+function  calculateResult() {
+  // eval() ek built-in JavaScript function hai jo ek string ko JavaScript code mein evaluate karta hai. Iska basic syntax yeh hai:
+  
+  int.value = eval(int.value)
 
-function clear(a) {
-    
-   
-    
 }
-    
